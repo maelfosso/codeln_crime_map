c1ff114cd19baed9f802d746bf11d1292a981dcc
Mael FOSSO <fosso.mael.elvis@gmail.com>
2020-07-04 18:37:33 +0100

    SplashScreen added

---
+++ lib/splash_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('Crime Map')),
+    );
+  }
+}
